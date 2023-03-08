73434d1c5a32356d35089126fcdf6e2b8faa55d9
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-08 00:27:30 -0500

    added def__str__ to Resource in models.py; added first sample resource through site using add_resource

--- books/models.py
+++ books/models.py
@@ -28,3 +28,6 @@ class Resource(models.Model):
     media_type = models.CharField(choices=MEDIA_TYPES, max_length=50)
     url = models.URLField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} // {self.media_type}'
