1e9e206059670f6e93eb97e7fb317f72a76e52a2
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-08 20:51:01 -0500

    commented out '... HttpResponse' in views.py; tried adding header with link to login in base.html--not working

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Resource
 from .forms import ResourceForm
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 @login_required
